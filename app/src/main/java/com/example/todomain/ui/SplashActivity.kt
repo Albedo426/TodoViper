fcd72820320c107e4d3a7b248546ad5053704e84
fatih Yılmaz <fatikyilmaz@hotmail.com>
2022-01-28 14:16:43 +0300

    added loti

--- app/src/main/java/com/example/todomain/ui/SplashActivity.kt
+++ app/src/main/java/com/example/todomain/ui/SplashActivity.kt
@@ -23,11 +23,8 @@ class SplashActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         supportActionBar?.hide()
-
         setContentView(R.layout.activity_splash)
-
         mDelayHandler = Handler()
         mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
 
